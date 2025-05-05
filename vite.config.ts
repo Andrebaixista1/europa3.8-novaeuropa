a65befcef9c9588ecf37bb06583d5fcde21a6396
unknown <andrbter@gmail.com>
2025-05-05 18:19:37 -0300

    outro vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+// vite.config.ts ou vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// ⚠️ TUDO deve estar dentro do defineConfig!
 export default defineConfig({
-  base: "./",
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://177.153.62.236:5678',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });
